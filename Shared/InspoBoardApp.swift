53e53070152e5326161593818429323780804d3a
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-08-03 16:18:12 -0700

    Initial Commit

---
+++ Shared/InspoBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspoBoardApp.swift
+//  Shared
+//
+//  Created by Kelsey Steven on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct InspoBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
